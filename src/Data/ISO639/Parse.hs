75531a9aaa69080ba825d9e0d4b68e21b7cd8eda
Kirk Peterson <necrobious@gmail.com>
2013-06-05 12:32:17 -0700

    changed ISO639_2 name to Language, since it makes more sense

--- src/Data/ISO639/Parse.hs
+++ src/Data/ISO639/Parse.hs
@@ -5,27 +5,27 @@ module Data.ISO639.Parse
   ) where
 
 import qualified Data.HashMap.Strict as Map
-import Data.ISO639.Types (ISO639_2, alpha3_terminologic, alpha3_bibliographic, iso639_2s, alpha2)
+import Data.ISO639.Types (Language, alpha3_terminologic, alpha3_bibliographic, languages, alpha2)
 import Data.Text(Text)
 
-idx_a3b = foldr fld Map.empty iso639_2s
+idx_a3b = foldr fld Map.empty languages
   where
   fld iso acc = Map.insert (alpha3_bibliographic iso) iso acc
 
-idx_a3t = foldr fld Map.empty iso639_2s
+idx_a3t = foldr fld Map.empty languages
   where
   fld iso acc = case alpha3_terminologic iso of {Nothing -> acc ; Just a3 -> Map.insert a3 iso acc}
        
-idx_a2  = foldr fld Map.empty iso639_2s
+idx_a2  = foldr fld Map.empty languages
   where
   fld iso acc = case alpha2 iso of {Nothing -> acc ; Just a3 -> Map.insert a3 iso acc}
 
-parseAlpha3Bibliographic :: Text -> Maybe ISO639_2
+parseAlpha3Bibliographic :: Text -> Maybe Language
 parseAlpha3Bibliographic input = Map.lookup input idx_a3b
   
-parseAlpha3Terminologic :: Text -> Maybe ISO639_2
+parseAlpha3Terminologic :: Text -> Maybe Language
 parseAlpha3Terminologic input = Map.lookup input idx_a3t
 
-parseAlpha2 :: Text -> Maybe ISO639_2
+parseAlpha2 :: Text -> Maybe Language
 parseAlpha2 input = Map.lookup input idx_a2
 
